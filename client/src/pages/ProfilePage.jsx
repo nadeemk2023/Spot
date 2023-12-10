8a9c86db0305b20bdfa06e272e7808759f0f061a
alexis gray <alexisjgray@yahoo.com>
2023-12-10 11:34:54 -0800

    error at index.tsx but cannot find index.tsx

---
+++ client/src/pages/ProfilePage.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import EditProfile from "../components/EditProfile/EditProfile";
+
+function ProfilePage() {
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-4 mb-4">
+          <img
+            src=""
+            alt="Profile Picture"
+            className="img-fluid rounded-circle"
+          />
+        </div>
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">About Us</h5>
+              <p>
+                This is where the user will write about their pupum and
+                themselves.
+              </p>
+            </div>
+          </div>
+          <Link to={<EditProfile />} className="btn btn-primary mt-3">
+            Edit Profile
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfilePage;
