3d3fe4164c6caff60bc2df1e334089a31ed8eeff
nadeemk2023 <nadeemekhan@gmail.com>
2023-12-10 22:19:20 -0800

    Moved imports around and fixed folder architecture

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import Header from './components/Header';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  Navigate,
-} from 'react-router-dom';
-import LandingPage from './components/pages/LandingPage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import Login from '../components/Login/Login';
+import Header from '../components/Header';
 
 const LandingPage = () => {
+  const styleCarousel = {
+    border: '2px solid black',
+    padding: '10px',
+    margin: '10px',
+    color: 'darkred',
+    height: '500px',
+    width: '500px',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  };
   return (
-    <div className="landing-page">
-      <div>This will be the carousel later</div>
+    <div>
+      <Header />
+      <div style={styleCarousel}>The carousel will be here later</div>
+      <Login />
     </div>
   );
 };
