87eabaf18b5e6bbd7fcb7a65f4c5b17e2ecbec4a
alexis gray <alexisjgray@yahoo.com>
2023-12-11 10:50:39 -0800

    took out routes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,11 @@
 import "./App.css";
 import CustomNavbar from "./components/Navbar/Navbar";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import { Home, Profile, Search, Feed } from "./pages";
 
 function App() {
   return (
     <Router>
       <CustomNavbar />
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/profile" element={<Profile />} />
-      <Route exact path="/search" element={<Search />} />
-      <Route exact path="/feed" element={<Feed />} />
-      <Route exact path="/" element={<Logout />} />
     </Router>
   );
 }
