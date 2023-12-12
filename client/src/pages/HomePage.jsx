fdd932992bfbd6ca09dee63397f4af64a142398e
nadeemk2023 <nadeemekhan@gmail.com>
2023-12-12 14:07:05 -0800

    Added homepage route to App.jsx, still working on homePage

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div style={{ margin: 0, padding: 0 }}>
+      <h3>HomePage</h3>
+    </div>
+  );
+};
+
+export default HomePage;
