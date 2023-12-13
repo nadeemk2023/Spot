99196de1bb207defa3d11e649f1aaab15f017eb9
Karen Schick <karen.schick6@gmail.com>
2023-12-13 15:34:17 -0500

    fixed browser router issues, remove strictmode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {  Route, Routes } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import RegisterPage from "./pages/RegisterPage";
 import React from "react";
@@ -8,13 +8,13 @@ import ErrorBoundary from "./components/ErrorBoundary";
 function App() {
   return (
     <ErrorBoundary>
-      <Router>
+      
         <Routes>
           <Route path="/" element={<LandingPage />} />
           <Route path="/signup" element={<RegisterPage />} />
           //! Add other Routes here
         </Routes>
-      </Router>
+      
     </ErrorBoundary>
   );
 }
