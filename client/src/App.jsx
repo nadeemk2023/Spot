8119b1df029c7f1411ab373929b64dd77300fba7
nadeemk2023 <nadeemekhan@gmail.com>
2023-12-13 21:38:13 -0800

    added homepage route to app.jsx to continue working on it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,20 @@
-import "./App.css";
-import {  Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import RegisterPage from "./pages/RegisterPage";
-import React from "react";
-import ErrorBoundary from "./components/ErrorBoundary";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import RegisterPage from './pages/RegisterPage';
+import React from 'react';
+import ErrorBoundary from './components/ErrorBoundary';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <ErrorBoundary>
-      
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<RegisterPage />} />
-          //! Add other Routes here
-        </Routes>
-      
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<RegisterPage />} />
+        <Route path="/homepage" element={<HomePage />} />
+        {/* Add other Routes here */}
+      </Routes>
     </ErrorBoundary>
   );
 }
