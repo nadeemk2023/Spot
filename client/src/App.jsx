ed25ddb8e0ef6006445c097593d83f560c467342
alexis gray <alexisjgray@yahoo.com>
2023-12-14 12:25:36 -0800

    commit before pull

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,36 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import RegisterPage from "./pages/RegisterPage";
+import { BrowserRouter as Route, Routes } from "react-router-dom";
+import { LandingPage, RegisterPage } from "./pages";
 import React from "react";
 import ErrorBoundary from "./components/ErrorBoundary";
+import CustomNavbar from "./components/Navbar/Navbar";
+import { ProvideAuth, useProvideAuth, useAuth } from "./hooks/useAuth";
 
 function App() {
+  const {
+    state: { user },
+  } = useProvideAuth();
+
   return (
-    <ErrorBoundary>
-      <Router>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/signup" element={<RegisterPage />} />
-          //! Add other Routes here
-        </Routes>
-      </Router>
-    </ErrorBoundary>
+    <ProvideAuth>
+      <ErrorBoundary>
+        {/* if user is logged in then home page will show */}
+        {user ? (
+          <Routes>
+            {/* <Route path="/" element={<LandingPage />} /> */}
+            {/* <Route path="/signup" element={<RegisterPage />} /> */}
+            <Route path="/navbar" element={<CustomNavbar />} />
+            //! Add other Routes here
+          </Routes>
+        ) : (
+          // if no user then only the landing page will show
+          <Routes>
+            <Route path="/signup" element={<RegisterPage />} />
+            <Route path="/" element={<LandingPage />} />
+          </Routes>
+        )}
+      </ErrorBoundary>
+    </ProvideAuth>
   );
 }
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as LandingPage } from "./LandingPage";
+export { default as RegisterPage } from "./RegisterPage";
+export { default as HomePage } from "./HomePage";
