13b2ec895a096d9251e851704a216639cf3d861d
alexis gray <alexisjgray@yahoo.com>
2023-12-28 10:57:11 -0800

    commit to add this component to editprofile component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,7 @@ import React from "react";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ProvideAuth, useProvideAuth, useAuth } from "./hooks/useAuth";
 import CustomNavbar from "./components/CustomNavbar/CustomNavbar";
-import UploadFile from "./components/UploadFile/UploadFile";
- import ParkPage from "./pages/ParkPage";
+import ParkPage from "./pages/ParkPage";
 
 function App() {
   const {
@@ -27,7 +26,6 @@ function App() {
           <Route path="/signup" element={<RegisterPage />} />
           <Route path="/home" element={<HomePage />} />
           <Route path="/profile/u/:uname" element={<ProfilePage />} />
-          <Route path="upload" element={<UploadFile />} />
           <Route path="/parks" element={<ParkPage />} />
           <Route path="/search" element={<SearchPage />} />
           //! Add other Routes here
--- client/src/components/UploadFile/UploadFile.jsx
+++ client/src/components/UploadFile/UploadFile.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-// import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import api from "../../../utils/api.utils";
 
@@ -7,88 +6,89 @@ const UploadFile = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
 
   const handleFileUpload = (event) => {
-    setSelectedFiles(event.target.files[0]);
+    setSelectedFiles([...selectedFiles, ...event.target.files]);
   };
 
   const handleDrop = (event) => {
     event.preventDefault();
     event.stopPropagation();
-    setSelectedFiles(event.target.files[0]);
+    setSelectedFiles([...selectedFiles, ...event.target.files]);
   };
 
   const handleUpload = () => {
     const formData = new FormData();
-    // selectedFiles.forEach((file) => {
-    formData.append("files", selectedFiles);
+    selectedFiles.forEach((file) => {
+      formData.append("files", file);
 
-    api
-      .post("/files/images", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
-      })
-      .then((res) => {
-        res.data;
-        console.log(res.data);
-      })
-      .catch((err) => {
-        err;
-      });
-  };
+      api
+        .post("/files/images", formData, {
+          headers: { "Content-Type": "multipart/form-data" },
+        })
+        .then((res) => {
+          res.data;
+          console.log(res.data);
+        })
+        .catch((err) => {
+          err;
+        });
+    });
 
-  const removeFile = (index) => {
-    const newFiles = [...selectedFiles];
-    newFiles.splice(index, 1);
-    setSelectedFiles(newFiles);
-  };
+    const removeFile = (index) => {
+      const newFiles = [...selectedFiles];
+      newFiles.splice(index, 1);
+      setSelectedFiles(newFiles);
+    };
 
-  const preventDefaults = (event) => {
-    event.preventDefault();
-    event.stopPropagation();
-  };
+    const preventDefaults = (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+    };
 
-  return (
-    <div
-      className="container mt-5 p-3 border border-primary rounded"
-      onDrop={handleDrop}
-      onDragOver={preventDefaults}
-      onDragEnter={preventDefaults}
-    >
-      <h3>Upload File</h3>
-      <div className="mb-3">
-        <label htmlFor="fileInput" className="form-label">
-          Drag and drop files here or click to browse.
-        </label>
-        <input
-          type="file"
-          id="fileInput"
-          className="form-control"
-          multiple
-          onChange={handleFileUpload}
-        />
-      </div>
-      <div>
-        <h5>Selected Files:</h5>
-        <ul className="list-group">
-          {/* {selectedFiles.map((file, index) => (
-            <li
-              key={index}
-              className="list-group-item d-flex justify-content-between align-items-center"
-            >
-              {file.name}
-              <button
-                className="btn btn-danger btn-sm"
-                onClick={() => removeFile(index)}
+    return (
+      <div
+        className="container mt-5 p-3 border border-primary rounded"
+        onDrop={handleDrop}
+        onDragOver={preventDefaults}
+        onDragEnter={preventDefaults}
+      >
+        <h3>Upload File</h3>
+        <div className="mb-3">
+          <label htmlFor="fileInput" className="form-label">
+            Drag and drop files here or click to browse.
+          </label>
+          <input
+            type="file"
+            id="fileInput"
+            className="form-control"
+            multiple
+            onChange={handleFileUpload}
+          />
+        </div>
+        <div>
+          <h5>Selected Files:</h5>
+          <ul className="list-group">
+            {selectedFiles.map((file, index) => (
+              <li
+                key={index}
+                className="list-group-item d-flex justify-content-between align-items-center"
               >
-                X
-              </button>
-            </li>
-          ))} */}
-        </ul>
+                {file.name}
+                <button
+                  className="btn btn-danger btn-sm"
+                  onClick={() => removeFile(index)}
+                >
+                  X
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button className="btn btn-primary mt-3" onClick={handleUpload}>
+          Upload
+        </button>
       </div>
-      <button className="btn btn-primary mt-3" onClick={handleUpload}>
-        Upload
-      </button>
-    </div>
-  );
+    );
+  };
 };
 
 export default UploadFile;
