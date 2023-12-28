3408bdc41805150db94c9fe2f0a553e9a83a51b7
Karen Schick <karen.schick6@gmail.com>
2023-12-28 14:03:46 -0500

    changed location radius

--- client/src/components/ParkLocator/ParkLocator.jsx
+++ client/src/components/ParkLocator/ParkLocator.jsx
@@ -4,61 +4,62 @@ import { Card, Button } from "react-bootstrap";
 import axios from "axios";
 
 function ParkLocator() {
-    const [dogParks, setDogParks] = useState([]);
+  const [dogParks, setDogParks] = useState([]);
 
-    function getCurrentLocation() {
-      return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition((position) => {
-          const coords = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude,
-          };
-          resolve(coords);
-        }, reject);
-      });
-    }
-  
-    const fetchParks = async () => {
-      try {
-        const userCoords = await getCurrentLocation();
-        const options = {
-          method: "POST",
-          url: "https://pipican-dog-park-and-dog-beach-locator-api.p.rapidapi.com/nearby-basic",
-          headers: {
-            "content-type": "application/json",
-            "X-RapidAPI-Key":
-              "a8cae885b6msha39d1a7a8eccfd1p1759bajsn2ffd7e679a1f",
-            "X-RapidAPI-Host":
-              "pipican-dog-park-and-dog-beach-locator-api.p.rapidapi.com",
-          },
-          data: {
-            coords: userCoords,
-            radius: 1,
-            leisure: "dog_park",
-          },
+  function getCurrentLocation() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const coords = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
         };
-  
-        const response = await axios.request(options);
-        console.log("API response", response.data)
-        setDogParks(response.data.result || []);
-        console.log("dog parks", response.data.result);
-      } catch (error) {
-        console.error(error);
-        setDogParks([])
-      }
-    };
-  
-  const handleClick = () => {
-    fetchParks()
+        resolve(coords);
+      }, reject);
+    });
   }
-  
-    return (
-      <div className="mt-5">
-        <button onClick={handleClick}>Search Dog Parks</button>
-  
-        <div>
-          {dogParks && dogParks.length > 0 ? (dogParks.map((park, index) => (
-            <Card key={index} >
+
+  const fetchParks = async () => {
+    try {
+      const userCoords = await getCurrentLocation();
+      const options = {
+        method: "POST",
+        url: "https://pipican-dog-park-and-dog-beach-locator-api.p.rapidapi.com/nearby-basic",
+        headers: {
+          "content-type": "application/json",
+          "X-RapidAPI-Key":
+            "a8cae885b6msha39d1a7a8eccfd1p1759bajsn2ffd7e679a1f",
+          "X-RapidAPI-Host":
+            "pipican-dog-park-and-dog-beach-locator-api.p.rapidapi.com",
+        },
+        data: {
+          coords: userCoords,
+          radius: 4,
+          leisure: "dog_park",
+        },
+      };
+
+      const response = await axios.request(options);
+      console.log("API response", response.data);
+      setDogParks(response.data.parks || []);
+      console.log("dog parks", response.data.parks);
+    } catch (error) {
+      console.error(error);
+      setDogParks([]);
+    }
+  };
+
+  const handleClick = () => {
+    fetchParks();
+  };
+
+  return (
+    <div className="mt-5">
+      <button onClick={handleClick}>Search Dog Parks</button>
+
+      <div>
+        {dogParks && dogParks.length > 0 ? (
+          dogParks.map((park, index) => (
+            <Card key={index}>
               <Card.Body>
                 <Card.Title>{park.name}</Card.Title>
                 <Card.Text>{park.address}</Card.Text>
@@ -66,11 +67,13 @@ function ParkLocator() {
                 <Button variant="primary">Visit Park</Button>
               </Card.Body>
             </Card>
-          )) ) : (<div>No Parks found</div>)}
-        </div>
+          ))
+        ) : (
+          <div>No Parks found</div>
+        )}
       </div>
-    );
-  };
+    </div>
+  );
+}
 
-
-export default ParkLocator
+export default ParkLocator;
