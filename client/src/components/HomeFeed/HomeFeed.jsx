8b31b3118cead20d41b15ab19d22dbc197348ff8
nadeemk2023 <nadeemekhan@gmail.com>
2023-12-28 21:25:34 -0800

    Date functionality added to posts on homeFeed

--- client/src/components/HomeFeed/HomeFeed.jsx
+++ client/src/components/HomeFeed/HomeFeed.jsx
@@ -16,7 +16,6 @@ const HomeFeed = () => {
     try {
       const response = await api.get('/posts');
       if (response.data && response.data.length > 0) {
-        console.log(response.data);
         setPosts(response.data);
       } else {
         console.log('No posts found');
