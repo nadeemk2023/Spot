c6c7d102a2ae7728db30f0eb65c24ed8b8ca3545
alexis gray <alexisjgray@yahoo.com>
2024-01-02 11:35:01 -0800

    styled navbar

--- client/src/components/CustomNavbar/CustomNavbar.jsx
+++ client/src/components/CustomNavbar/CustomNavbar.jsx
@@ -15,42 +15,50 @@ function CustomNavbar() {
   }
 
   return (
-    <Navbar className="bg-body-tertiary " fixed="top">
-      <Container>
+    <Navbar className="bg-body-tertiary py-0" fixed="top">
+      <Container fluid>
         <Navbar.Brand as={Link} to="/home">
           <img
             alt="white dog with black bullseye"
             src={Logo}
-            width="30"
-            height="30"
+            width="100"
+            height="100"
             className="d-inline-block align-top"
           />
         </Navbar.Brand>
         <Navbar.Toggle />
-        <Navbar.Collapse className="">
-          <Nav className="ml-auto">
+        <Navbar.Collapse>
+          <Nav className="ml-auto w-100 justify-content-around">
             {user ? (
               <Nav.Link
                 as={Link}
                 to={`/profile/u/${user.username}`}
-                className="mx-4"
+                className="text-decoration-none fs-5"
               >
                 Profile
               </Nav.Link>
             ) : (
               ""
             )}
-            <Nav.Link as={Link} to="/search" className="mx-4">
+            <Nav.Link
+              as={Link}
+              to="/search"
+              className="text-decoration-none fs-5"
+            >
               Search
             </Nav.Link>
-            <Nav.Link as={Link} to="/feed" className="mx-4">
+            <Nav.Link
+              as={Link}
+              to="/feed"
+              className="text-decoration-none fs-5"
+            >
               Feed
             </Nav.Link>
             <Nav.Link
               as={Link}
               to="/"
               onClick={() => signout()}
-              className="mx-4"
+              className="text-decoration-none fs-5"
             >
               Logout
             </Nav.Link>
