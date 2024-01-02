ef07465f4512e106b9b4d26952701617c9f14a26
nadeemk2023 <nadeemekhan@gmail.com>
2024-01-02 10:58:48 -0800

    Edit functionality incorporated and re-renders live on click of "save changes"

--- client/src/components/PostCard/PostCard.jsx
+++ client/src/components/PostCard/PostCard.jsx
@@ -7,7 +7,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart as filledHeart } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as outlinedHeart } from '@fortawesome/free-regular-svg-icons';
 
-const PostCard = ({ post, onDelete, onEdit }) => {
+const PostCard = ({ post }) => {
   const {
     state: { user: currentUser },
   } = useProvideAuth();
@@ -17,6 +17,8 @@ const PostCard = ({ post, onDelete, onEdit }) => {
   const [isLiked, setIsLiked] = useState(
     post.likes.some(like => like._id === currentUser.uid)
   );
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedText, setEditedText] = useState('');
 
   const timeAgo = formatDistanceToNow(parseISO(post.createdAt), {
     addSuffix: true,
@@ -45,6 +47,23 @@ const PostCard = ({ post, onDelete, onEdit }) => {
     try {
       const res = await api.put('/posts/comments', responseData);
       console.log(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const handleEditComment = async () => {
+    const responseData = {
+      text: editedText,
+      userid: currentUser.uid,
+    };
+    try {
+      const res = await api.put(`/posts/${post._id}`, responseData);
+      if (res.status === 200) {
+        setPostState(res.data);
+        setIsEditing(false);
+      } else {
+        console.error('Failed to update post:', res.status);
+      }
     } catch (err) {
       console.error(err);
     }
@@ -80,7 +99,7 @@ const PostCard = ({ post, onDelete, onEdit }) => {
             <div>
               <Button
                 variant="outline-secondary"
-                onClick={() => onEdit(post)}
+                onClick={() => setIsEditing(true)}
                 className="mr-2"
               >
                 Edit
@@ -94,7 +113,17 @@ const PostCard = ({ post, onDelete, onEdit }) => {
             </div>
           )}
         </div>
-        <Card.Text className="mb-3">{post.text}</Card.Text>
+        {!isEditing ? (
+          <Card.Text className="mb-3">{postState.text}</Card.Text>
+        ) : (
+          <>
+            <input
+              value={editedText}
+              onChange={e => setEditedText(e.target.value)}
+            ></input>
+            <Button onClick={() => handleEditComment()}>Save Change</Button>
+          </>
+        )}
         <div className="mb-3 d-flex justify-content-start align-items-center">
           <span role="img" aria-label="thumbs up" className="mr-2">
             👍 {postState.likes.length} Likes
