a03b4a8e065a14507d411206491ce4b6b212cc2a
nadeemk2023 <nadeemekhan@gmail.com>
2024-01-06 21:57:33 -0800

    Removing some erroneous comments in code

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -16,15 +16,13 @@ const HomePage = () => {
   const [refreshKey, setRefreshKey] = useState(0);
 
   const refreshPosts = useCallback(() => {
-    setRefreshKey((prevKey) => prevKey + 1);
+    setRefreshKey(prevKey => prevKey + 1);
   }, []);
 
   return (
     <>
       <div>
-        {/* Main Content */}
         <Container className="mt-4">
-          {/* Create Post Panel */}
           <Row className="d-flex justify-content-center ">
             <Col xs={10} className="mb-4" style={{ border: "1px solid black" }}>
               <div className="bg-light p-3">
@@ -34,12 +32,10 @@ const HomePage = () => {
             </Col>
           </Row>
 
-          {/* Feed Component */}
           <Row className="d-flex justify-content-center">
             <Col xs={10} style={{ border: "1px solid black" }}>
               <div className="bg-light p-3">
                 <h4>Feed</h4>
-                {/* Placeholder for Feed component */}
                 <HomeFeed key={refreshKey} />
               </div>
             </Col>
