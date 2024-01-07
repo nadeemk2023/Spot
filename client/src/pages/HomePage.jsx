5272aa787b4fb50f1e79aa354c278eb208a47ed7
nadeemk2023 <nadeemekhan@gmail.com>
2024-01-06 23:05:30 -0800

    Added searchBar and parkLocator to homePage as sticky elements that are filling the white space on page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import CreatePost from "../components/CreatePost/CreatePost";
 import HomeFeed from "../components/HomeFeed/HomeFeed";
 import { ParkContext } from "../components/ParkLocator/ParkLocatorContext";
 import ParkLocator from "../components/ParkLocator/ParkLocator";
+import SearchBar from "../components/SearchBar/SearchBar";
 
 const HomePage = () => {
   const {
@@ -20,31 +21,55 @@ const HomePage = () => {
   }, []);
 
   return (
-    <>
-      <div>
-        <Container className="mt-4">
-          <Row className="d-flex justify-content-center ">
-            <Col xs={10} className="mb-4" style={{ border: "1px solid black" }}>
-              <div className="bg-light p-3">
-                <h4>Create Post</h4>
-                <CreatePost onPostCreated={refreshPosts} />
-              </div>
-            </Col>
-          </Row>
-
-          <Row className="d-flex justify-content-center">
-            <Col xs={10} style={{ border: "1px solid black" }}>
-              <div className="bg-light p-3">
-                <h4>Feed</h4>
-                <HomeFeed key={refreshKey} />
-              </div>
-            </Col>
-          </Row>
-        </Container>
+    <Container
+      fluid
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        paddingTop: "20px",
+        position: "relative",
+        height: "fit-content",
+      }}
+    >
+      <div
+        style={{
+          width: "250px",
+          position: "sticky",
+          top: "35%",
+          alignSelf: "flex-start",
+          marginLeft: "calc(20% - 125px)",
+        }}
+      >
+        <SearchBar />
       </div>
 
-      <ParkLocator showResultsInModal={true} />
-    </>
+      <div style={{ flex: "0 1 800px", margin: "0 auto" }}>
+        {" "}
+        <Row className="justify-content-center mb-4">
+          <Col>
+            <CreatePost onPostCreated={refreshPosts} />
+          </Col>
+        </Row>
+        <Row className="justify-content-center">
+          <Col>
+            <HomeFeed key={refreshKey} />
+          </Col>
+        </Row>
+      </div>
+
+      <div
+        style={{
+          width: "250px",
+          position: "sticky",
+          top: "35%",
+          alignSelf: "flex-start",
+          marginRight: "calc(20% - 125px)",
+        }}
+      >
+        <ParkLocator showResultsInModal={true} />
+      </div>
+    </Container>
   );
 };
 
